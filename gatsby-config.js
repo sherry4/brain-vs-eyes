944c0041633b5433ad2959758cc2e54407ef12b8
L\Admin <hxuan.lam4@gmail.com>
2021-09-14 09:16:14 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Brain vs Eyes",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
+};
